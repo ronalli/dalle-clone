d8b3499c56cba555f53f6e2d3369a26bca2eb30d
Nikita <khakimov.nikita.vch@gmail.com>
2023-03-29 19:38:08 +0300

    serverDallE

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary';
+
+import PostSchema from '../mongodb/model/post.js';
+
+dotenv.config();
+
+const router = express.Router();
